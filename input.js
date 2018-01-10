b8893f52d4cc5aee466153eacf2c4f1862e5f143
Ben <benferree3@gmail.com>
2018-01-09 19:22:59 -0700

    Create input.js

---
+++ input.js
@@ -0,0 +1,7 @@
+var form = document.querySelector("form");
+form.addEventListener("submit", function(event) {
+    //console.log("Saving value", form.elements.value.value);
+    input = form.elements.value.value;
+    event.preventDefault();
+    test();
+});
