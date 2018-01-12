ee25f6887a235eabbc1180f51ba6ce7362749960
Ben <benferree3@gmail.com>
2018-01-12 15:15:08 -0700

    Update form.js

--- form.js
+++ form.js
@@ -6,7 +6,7 @@
     var params = document.location.search.slice(1).split("&");
     for (var i = 0; i < params.length; i++) {
         var param = params[i].split("=");
-        var name = dec(param[0]), val = dec(param[1]);
+        var name = dec(param[1]), val = dec(param[2]);
             console.log(name);
             console.log(params);
             console.log(param);
