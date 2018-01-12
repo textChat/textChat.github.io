f01417fc2db65dab6803e474146c5f8c64f46b22
Ben <benferree3@gmail.com>
2018-01-12 06:39:06 -0700

    Update sendData.php

--- sendData.php
+++ sendData.php
@@ -1,6 +1,22 @@
 <?php include("conn.php");
 
 $username = $_GET['username'];
+if(empty($username)||($email)||($password)){
+  header("Location: ../signup.html?signup=empty");
+  exit();
+}else{
+  if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+    header("Location: ../signup.html?signup=email");
+    exit();
+}else{
+  $sql = "SELECT * FROM users WHERE username='$username'";
+	$result = mysqli_query($conn, $sql);
+	$resultCheck = mysqli_num_rows($result);
 
-$sql = "INSERT INTO users (username) VALUES ('$username');";
+	if ($resultCheck > 0) {
+    header("Location: ../signup.html?signup=usertaken");
+	  exit();
+  }else{      
+$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password');";
 mysqli_query($conn, $sql);  
+  }
