c4d9686d2cdf99df7953ddfe163fac7ee8193a97
Ben <benferree3@gmail.com>
2018-01-12 06:23:48 -0700

    Update input.js

--- input.js
+++ input.js
@@ -2,14 +2,16 @@ var input;
 var form = document.querySelector("form");
 form.addEventListener("submit", function(event) {
     //console.log("Saving value", form.elements.value.value);
-    input = form.elements.value.value;
+    username = form.elements.value.value;
+    email = form.elements.value1.value1;
+    password = form.elements.value2.value2;
     event.preventDefault();
     test();
 });
 
 function test(){
-    alert(input);
+    console.log(username, email, password);
     xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "php/sendData.php?username=" + input, true);
+    xmlhttp.open("GET", "sendData.php?username=" + username, true);
     xmlhttp.send();
 }
